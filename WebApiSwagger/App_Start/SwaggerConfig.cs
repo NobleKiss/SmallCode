2093b81708f71b079cb6aa55c0196180378ec32d
PicaPica <495050266@qq.com>
2019-08-19 16:44:44 +0800

    修改API同名冲突

--- WebApiSwagger/App_Start/SwaggerConfig.cs
+++ WebApiSwagger/App_Start/SwaggerConfig.cs
@@ -2,6 +2,7 @@ using System.Web.Http;
 using WebActivatorEx;
 using WebApiSwagger;
 using Swashbuckle.Application;
+using System.Linq;
 
 [assembly: PreApplicationStartMethod(typeof(SwaggerConfig), "Register")]
 
@@ -32,7 +33,7 @@ namespace WebApiSwagger
                         // hold additional metadata for an API. Version and title are required but you can also provide
                         // additional fields by chaining methods off SingleApiVersion.
                         //
-                        c.SingleApiVersion("v1", "WebApiSwagger");
+                        c.SingleApiVersion("v1", "²âÊÔSwagger");
 
                         // If you want the output Swagger docs to be indented properly, enable the "PrettyPrint" option.
                         //
@@ -61,7 +62,7 @@ namespace WebApiSwagger
                         //c.BasicAuth("basic")
                         //    .Description("Basic HTTP Authentication");
                         //
-						// NOTE: You must also configure 'EnableApiKeySupport' below in the SwaggerUI section
+                        // NOTE: You must also configure 'EnableApiKeySupport' below in the SwaggerUI section
                         //c.ApiKey("apiKey")
                         //    .Description("API Key Authentication")
                         //    .Name("apiKey")
@@ -102,7 +103,7 @@ namespace WebApiSwagger
                         // more Xml comment files.
                         //
                         //c.IncludeXmlComments(GetXmlCommentsPath());
-
+                        c.IncludeXmlComments(string.Format("{0}/bin/WebApiSwagger.xml", System.AppDomain.CurrentDomain.BaseDirectory));
                         // Swashbuckle makes a best attempt at generating Swagger compliant JSON schemas for the various types
                         // exposed in your API. However, there may be occasions when more control of the output is needed.
                         // This is supported through the "MapType" and "SchemaFilter" options:
@@ -170,7 +171,7 @@ namespace WebApiSwagger
                         // with the same path (sans query string) and HTTP method. You can workaround this by providing a
                         // custom strategy to pick a winner or merge the descriptions for the purposes of the Swagger docs
                         //
-                        //c.ResolveConflictingActions(apiDescriptions => apiDescriptions.First());
+                        c.ResolveConflictingActions(apiDescriptions => apiDescriptions.First());
 
                         // Wrap the default SwaggerGenerator with additional behavior (e.g. caching) or provide an
                         // alternative implementation for ISwaggerProvider with the CustomProvider option.
---
+++ WebApiSwagger/Controllers/DefaultController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApiSwagger.Controllers
+{
+    public class DefaultController : ApiController
+    {
+        // GET: api/Default
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Default/5
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
+        /// <summary>
+        /// 根据输入的字符串返回.
+        /// </summary>
+        /// <param name="id">需要输入的id</param>
+        /// <param name="str">需要输入的字符串</param>
+        /// <returns>拼接后的字符串</returns>
+        public string GetStr(int id,string str)
+        {
+            return "当前输入的是字符串是：" + str + "Id是：" + id;
+        }
+        // POST: api/Default
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Default/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Default/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
